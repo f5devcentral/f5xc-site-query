a75ca90e0a9a6082a0152498a45910310112ecee
c.klewar <c.klewar@f5.com>
2025-03-06 16:11:31 +0100

    add exception handling if input file not exists

--- lib/api.py
+++ lib/api.py
@@ -334,167 +334,161 @@ class Api(object):
         self.logger.info(f"Compare old site: {old_site} --> {old_file}")
         self.logger.info(f"Compare new site: {new_site} --> {new_file}")
 
-        data_old = None
-        data_new = None
-
-        try:
-            data_old = self.read_json_file(old_file)
-            data_new = self.read_json_file(new_file)
-
-        except KeyError as ke:
-            self.logger.info(f"site not found error: {ke}")
+        data_old = self.read_json_file(old_file)
+        data_new = self.read_json_file(new_file)
 
         self.logger.debug(f"DATA_OLD: {data_old}")
         self.logger.debug(f"DATA_NEW: {data_new}")
 
-        compared = diff(data_old['site'][old_site], data_new['site'][new_site], syntax="compact")
+        if data_old and data_new:
+            compared = diff(data_old['site'][old_site], data_new['site'][new_site], syntax="compact")
 
-        def get_by_path(root: dict | list = None, items: list = None, resp: list = None) -> list[str] | None:
-            """
-            Traverse a nested object by a sequence of path items.
-            :param root: the dict or list of values to obtain values from leveraging a path
-            :param items: list of items building a path. traverse root according to path and get value
-            :param resp: list where processed items will be appended to
-            :return: list of items obtained by path
-            """
+            def get_by_path(root: dict | list = None, items: list = None, resp: list = None) -> list[str] | None:
+                """
+                Traverse a nested object by a sequence of path items.
+                :param root: the dict or list of values to obtain values from leveraging a path
+                :param items: list of items building a path. traverse root according to path and get value
+                :param resp: list where processed items will be appended to
+                :return: list of items obtained by path
+                """
 
-            if items:
-                while len(items) > 0:
-                    item = items[0]
-                    items.pop(0)
+                if items:
+                    while len(items) > 0:
+                        item = items[0]
+                        items.pop(0)
 
-                    if isinstance(root, list) and item.isdigit():
-                        get_by_path(root[int(item)], items, resp)
-                    elif isinstance(root, dict):
-                        new_root = root.get(int(item)) if item.isdigit() else root.get(item)
+                        if isinstance(root, list) and item.isdigit():
+                            get_by_path(root[int(item)], items, resp)
+                        elif isinstance(root, dict):
+                            new_root = root.get(int(item)) if item.isdigit() else root.get(item)
 
-                        if new_root:
-                            if isinstance(new_root, str):
-                                self.logger.debug(f"STRING: {new_root}")
-                                resp.append(new_root)
-                            elif isinstance(new_root, int):
-                                self.logger.debug(f"INT: {new_root}")
-                                resp.append(new_root)
-                            elif isinstance(root, list):
-                                self.logger.debug(f"LIST: {new_root}")
-                            elif isinstance(root, dict):
-                                self.logger.debug(f"DICT: {new_root}")
-                                if len(items) == 0:
-                                    _tmp = root.get(item)
-                                    # print(item, _tmp)
-                                    if type(_tmp) == list:
-                                        # If complete interface definition is missing add list of missing interfaces and not all the sub items.
-                                        if item == "interfaces":
-                                            ifaces = list()
+                            if new_root:
+                                if isinstance(new_root, str):
+                                    self.logger.debug(f"STRING: {new_root}")
+                                    resp.append(new_root)
+                                elif isinstance(new_root, int):
+                                    self.logger.debug(f"INT: {new_root}")
+                                    resp.append(new_root)
+                                elif isinstance(root, list):
+                                    self.logger.debug(f"LIST: {new_root}")
+                                elif isinstance(root, dict):
+                                    self.logger.debug(f"DICT: {new_root}")
+                                    if len(items) == 0:
+                                        _tmp = root.get(item)
+                                        # print(item, _tmp)
+                                        if type(_tmp) == list:
+                                            # If complete interface definition is missing add list of missing interfaces and not all the sub items.
+                                            if item == "interfaces":
+                                                ifaces = list()
 
-                                            for item in _tmp:
-                                                if "ethernet_interface" in item:
-                                                    ifaces.append(item["ethernet_interface"]["device"])
+                                                for item in _tmp:
+                                                    if "ethernet_interface" in item:
+                                                        ifaces.append(item["ethernet_interface"]["device"])
 
-                                            resp.append(ifaces)
-                                        elif item == "proxys":
-                                            print("ITEM:", item)
+                                                resp.append(ifaces)
+                                            elif item == "proxys":
+                                                print("ITEM:", item)
+                                            else:
+                                                resp.append(_tmp)
+                                        elif type(_tmp) == dict:
+                                            resp.append(list(new_root.keys()))
                                         else:
-                                            resp.append(_tmp)
-                                    elif type(_tmp) == dict:
-                                        resp.append(list(new_root.keys()))
+                                            self.logger.debug(f"DICT: {new_root}")
+                                            resp.append(list(new_root.keys()))
                                     else:
-                                        self.logger.debug(f"DICT: {new_root}")
-                                        resp.append(list(new_root.keys()))
+                                        get_by_path(new_root, items, resp)
                                 else:
-                                    get_by_path(new_root, items, resp)
+                                    self.logger.info(f"Unknown key: {type(root)}")
                             else:
-                                self.logger.info(f"Unknown key: {type(root)}")
+                                self.logger.debug(f"new root item: {item}, {type(item)}")
+                                self.logger.debug(f"root: {root}")
+                                self.logger.debug(f"root.get(): {root.get(item)}")
                         else:
-                            self.logger.debug(f"new root item: {item}, {type(item)}")
-                            self.logger.debug(f"root: {root}")
-                            self.logger.debug(f"root.get(): {root.get(item)}")
-                    else:
-                        self.logger.debug(f"Unknown: {root}")
-
-            return resp
-
-        def get_keys(parent_key: str = None, dictionary: dict = None, resp: list[str] = None) -> list[str] | None:
-            """
-            get_keys will compute list of strings, where each string represents path to key in a dict
-            :param parent_key: last key becomes parent key. When func called first time parent key will be None.
-            :param dictionary: holds compared data. With each recursion dictionary will present latest key values
-            :param resp: a list of strings. Each string represents a key path later used to access values in site inventory
-            :return: final list of all computed key path strings
-            """
-
-            if dictionary:
-                self.logger.debug(f"DICTIONARY: {dictionary}")
-                keys = list(dictionary.keys())
-
-                while len(keys) > 0:
-                    key = keys[0]
-                    keys.pop(0)
-
-                    if key not in ["sms"]:
-                        if type(key) is jsondiff.symbols.Symbol:
-                            if key.label == "delete":
-                                self.logger.debug(f"DELETE: {parent_key} -- {key} -- {dictionary.get(key)}")
-                                for item in dictionary.get(key):
-                                    if item == "namespaces":
-                                        for namespace in data_old['site'][old_site]['namespaces']:
-                                            if "loadbalancer" in data_old['site'][old_site]['namespaces'][namespace]:
-                                                for lb_type in c.F5XC_LOAD_BALANCER_TYPES:
-                                                    if data_old['site'][old_site]['namespaces'][namespace]['loadbalancer'].get(lb_type.split("_")[0]):
-                                                        resp.append(f"{item}/{namespace}/loadbalancer/{lb_type.split("_")[0]}")
-                                            if "origin_pools" in data_old['site'][old_site]['namespaces'][namespace]:
-                                                resp.append(f"{item}/{namespace}/origin_pools")
-                                            if "proxys" in data_old['site'][old_site]['namespaces'][namespace]:
-                                                resp.append(f"{item}/{namespace}/proxys")
-                                    self.logger.debug(f"APPEND NEW ITEM: {f"{parent_key}/{item}" if parent_key else f"{item}"}")
-                                    resp.append(f"{parent_key}/{item}" if parent_key else f"{item}")
-                            elif key.label == "replace":
-                                self.logger.debug(f"REPLACE: {parent_key} -- {key} -- {dictionary.get(key)}")
-                                resp.append(f"{parent_key}" if parent_key else f"{key}")
-                            else:
-                                self.logger.debug(f"UNKNOWN KEY: {key}")
-                        elif isinstance(dictionary.get(key), list):
-                            self.logger.debug(f"LIST: {parent_key} -- {key} -- {dictionary.get(key)}")
-                            resp.append(f"{parent_key}/{key}" if parent_key else f"{key}")
-                        else:
-                            if isinstance(dictionary.get(key), str):
-                                if key not in c.EXCLUDE_COMPARE_ATTRIBUTES:
-                                    self.logger.debug(f"STRING: {parent_key} -- {key} -- {dictionary.get(key)}")
-                                    resp.append(f"{parent_key}/{key}" if parent_key else f"{key}")
-                            elif isinstance(dictionary.get(key), int):
-                                self.logger.debug(f"INT: {parent_key} -- {key} -- {dictionary.get(key)}")
-                                resp.append(f"{parent_key}/{key}" if parent_key else f"{key}")
-                            elif isinstance(dictionary.get(key), dict):
-                                self.logger.debug(f"DICT: {key} -- {type(key)} -- {dictionary.get(key)}")
-                                get_keys(f"{parent_key}/{key}", dictionary.get(key), resp) if parent_key else get_keys(key, dictionary.get(key), resp)
-                            else:
-                                self.logger.debug(f"UNKNOWN KEY: {key} -- {type(dictionary.get(key))}")
+                            self.logger.debug(f"Unknown: {root}")
 
                 return resp
 
-        r = []
-        # build list of key paths
-        k1 = get_keys(None, compared, r)
-        table = PrettyTable()
-        table.set_style(TableStyle.SINGLE_BORDER)
-        table.field_names = ["path", "values"]
+            def get_keys(parent_key: str = None, dictionary: dict = None, resp: list[str] = None) -> list[str] | None:
+                """
+                get_keys will compute list of strings, where each string represents path to key in a dict
+                :param parent_key: last key becomes parent key. When func called first time parent key will be None.
+                :param dictionary: holds compared data. With each recursion dictionary will present latest key values
+                :param resp: a list of strings. Each string represents a key path later used to access values in site inventory
+                :return: final list of all computed key path strings
+                """
 
-        if self.site:
-            table.padding_width = 1
-            table.title = self.site
+                if dictionary:
+                    self.logger.debug(f"DICTIONARY: {dictionary}")
+                    keys = list(dictionary.keys())
 
-        for k in k1:
-            response = list()
-            # get list of items to be  added as table row data
-            r1 = get_by_path(data_old['site'][old_site], [k for k in k.split("/")], response)
+                    while len(keys) > 0:
+                        key = keys[0]
+                        keys.pop(0)
 
-            if r1:
-                check = list(map(lambda regex: re.match(regex, k), c.EXCLUDE_COMPARE_ATTRIBUTES))
-                if not any(check):
-                    table.add_row([k, r1[0]])
-                    table.add_divider()
+                        if key not in ["sms"]:
+                            if type(key) is jsondiff.symbols.Symbol:
+                                if key.label == "delete":
+                                    self.logger.debug(f"DELETE: {parent_key} -- {key} -- {dictionary.get(key)}")
+                                    for item in dictionary.get(key):
+                                        if item == "namespaces":
+                                            for namespace in data_old['site'][old_site]['namespaces']:
+                                                if "loadbalancer" in data_old['site'][old_site]['namespaces'][namespace]:
+                                                    for lb_type in c.F5XC_LOAD_BALANCER_TYPES:
+                                                        if data_old['site'][old_site]['namespaces'][namespace]['loadbalancer'].get(lb_type.split("_")[0]):
+                                                            resp.append(f"{item}/{namespace}/loadbalancer/{lb_type.split("_")[0]}")
+                                                if "origin_pools" in data_old['site'][old_site]['namespaces'][namespace]:
+                                                    resp.append(f"{item}/{namespace}/origin_pools")
+                                                if "proxys" in data_old['site'][old_site]['namespaces'][namespace]:
+                                                    resp.append(f"{item}/{namespace}/proxys")
+                                        self.logger.debug(f"APPEND NEW ITEM: {f"{parent_key}/{item}" if parent_key else f"{item}"}")
+                                        resp.append(f"{parent_key}/{item}" if parent_key else f"{item}")
+                                elif key.label == "replace":
+                                    self.logger.debug(f"REPLACE: {parent_key} -- {key} -- {dictionary.get(key)}")
+                                    resp.append(f"{parent_key}" if parent_key else f"{key}")
+                                else:
+                                    self.logger.debug(f"UNKNOWN KEY: {key}")
+                            elif isinstance(dictionary.get(key), list):
+                                self.logger.debug(f"LIST: {parent_key} -- {key} -- {dictionary.get(key)}")
+                                resp.append(f"{parent_key}/{key}" if parent_key else f"{key}")
+                            else:
+                                if isinstance(dictionary.get(key), str):
+                                    if key not in c.EXCLUDE_COMPARE_ATTRIBUTES:
+                                        self.logger.debug(f"STRING: {parent_key} -- {key} -- {dictionary.get(key)}")
+                                        resp.append(f"{parent_key}/{key}" if parent_key else f"{key}")
+                                elif isinstance(dictionary.get(key), int):
+                                    self.logger.debug(f"INT: {parent_key} -- {key} -- {dictionary.get(key)}")
+                                    resp.append(f"{parent_key}/{key}" if parent_key else f"{key}")
+                                elif isinstance(dictionary.get(key), dict):
+                                    self.logger.debug(f"DICT: {key} -- {type(key)} -- {dictionary.get(key)}")
+                                    get_keys(f"{parent_key}/{key}", dictionary.get(key), resp) if parent_key else get_keys(key, dictionary.get(key), resp)
+                                else:
+                                    self.logger.debug(f"UNKNOWN KEY: {key} -- {type(dictionary.get(key))}")
 
-        return table
+                    return resp
+
+            r = []
+            # build list of key paths
+            k1 = get_keys(None, compared, r)
+            table = PrettyTable()
+            table.set_style(TableStyle.SINGLE_BORDER)
+            table.field_names = ["path", "values"]
+
+            if self.site:
+                table.padding_width = 1
+                table.title = self.site
+
+            for k in k1:
+                response = list()
+                # get list of items to be  added as table row data
+                r1 = get_by_path(data_old['site'][old_site], [k for k in k.split("/")], response)
+
+                if r1:
+                    check = list(map(lambda regex: re.match(regex, k), c.EXCLUDE_COMPARE_ATTRIBUTES))
+                    if not any(check):
+                        table.add_row([k, r1[0]])
+                        table.add_divider()
+
+            return table
 
     def run(self) -> dict:
         """
