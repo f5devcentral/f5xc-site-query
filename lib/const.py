e5e939197ccb5fda7b261313f5cae945b5a229ee
Christian Klewar <c.klewar@f5.com>
2025-06-13 12:17:36 +0200

    fix logic in compare to check if new site has additional data inserted. Skip those inserted items in compare.

--- lib/const.py
+++ lib/const.py
@@ -58,9 +58,10 @@ F5XC_CREATOR_CLASS_MAURICE = "maurice"
 API_PROCESSORS = ["site", "vs", "lb", "proxy", "originpool", "bgp", "smg", "cloudconnect", "segment"]
 PROCESSOR_PACKAGE = "lib.processor"
 CSV_EXPORT_KEYS = ["spec", "efp", "fpp", "bgp", "smg", "spoke", "segments", "dc_cluster_group", "nodes", "namespaces"]
-EXCLUDE_COMPARE_ATTRIBUTES = ["serial", "asset_tag", "hw-serial-number", "spec/site_to_site_ipsec_connectivity"]
 COMPARE_REGEX_HW_INFO_CPU_FLAGS = "nodes/.*/hw_info/cpu/flags"
 COMPARE_REGEX_HW_INFO_USB = "nodes/.*/hw_info/usb"
+EXCLUDE_COMPARE_ATTRIBUTES = ["serial", "asset_tag", "hw-serial-number", "spec/site_to_site_ipsec_connectivity",
+                              COMPARE_REGEX_HW_INFO_USB]
 SITE_OBJECT_TYPE_SMS = "sms"
 SITE_OBJECT_TYPE_LEGACY = "legacy"
 SITE_OBJECT_PROCESSORS = ["site_details", "efp", "fpp", "dc_cluster_group", "cloudlink", "node_interfaces", "hw_info",
