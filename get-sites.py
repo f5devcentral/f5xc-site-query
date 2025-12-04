f8ea5571f60134f775292a827b677e2398a5fec5
Christian Klewar <c.klewar@f5.com>
2025-12-05 00:01:31 +0100

    remove csv inventory support

--- get-sites.py
+++ get-sites.py
@@ -113,12 +113,6 @@ def main():
             logger.info("--file option is required")
             sys.exit(1)
 
-        # CSV inventory
-        data = q.build_inventory_csv(json_file=args.file) if args.inventory_file_csv else None
-        if data:
-            q.write_string_file(args.inventory_file_csv, data.get_csv_string()) if args.inventory_file_csv and data else None
-            logger.info(f"\n\n{data.get_formatted_string('text')}\n") if args.inventory_table else None
-
         # stdout inventory
         data = q.build_inventory_csv(json_file=args.file) if args.inventory_table else None
         if data:
