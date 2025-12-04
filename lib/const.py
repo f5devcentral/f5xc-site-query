dbe7c9048081a92d3063d49a092d52a2047a21e5
Christian Klewar <c.klewar@f5.com>
2025-12-04 22:59:36 +0100

    re enable vs api processor
    ref: https://jira.f5net.com/browse/XC-1422

--- lib/const.py
+++ lib/const.py
@@ -67,8 +67,7 @@ SITE_TYPES = [SITES_KEY, VIRTUAL_SITES_KEY]
 #
 # Site query
 #
-#API_PROCESSORS = ["vs", "site", "lb", "proxy", "originpool", "bgp", "smg", "cloudconnect", "segment"]
-API_PROCESSORS = ["site", "lb", "proxy", "originpool", "bgp", "smg", "cloudconnect", "segment"]
+API_PROCESSORS = ["vs", "site", "lb", "proxy", "originpool", "bgp", "smg", "cloudconnect", "segment"]
 PROCESSOR_PACKAGE = "lib.processor"
 CSV_EXPORT_KEYS = ["spec", "efp", "fpp", "bgp", "smg", "spoke", "segments", "dc_cluster_group","nodes", "namespaces"]
 XLSX_SERVICE_EXPORT_KEYS = ["efp", "fpp", "bgp", "smg", "spoke", "segments", "dc_cluster_group", "proxy"]
